a9248a8ed06670edd3b27b56918c3244d8620c47
Alexandr Rihtarich <rihtarich.ayu@gmail.com>
2020-05-13 17:53:51 +0300

    reverse fahr-celsius

---
+++ example_4/reverse_fahr-celsius.js
@@ -0,0 +1,12 @@
+/**
+  * prints Fahrenheit-Celsius reverse table
+  * for fahr = 0, 20, ..., 300; floating-point version
+  */
+let fahr;
+
+console.log('Fahr\tCelsius');
+console.log('----\t-------');
+
+for (fahr = 300; fahr >= 0; fahr = fahr - 20) {
+  console.log(`${fahr}\t${((5.0 / 9.0) * (fahr - 32.0)).toFixed(1)}`);
+}
