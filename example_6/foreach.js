df45f6e91f2f6ed36a5daff469dfc885e88b0bba
Rihtarich <rihtarich.ayu@gmail.com>
2020-05-18 23:23:24 +0300

    foreach method

---
+++ example_6/foreach.js
@@ -0,0 +1,13 @@
+/**
+ * prints one letter per line
+ */
+process.stdin.on("readable", () => {
+  let chunk;
+  while (null !== (chunk = process.stdin.read())) {
+    chunk.forEach((char) => {
+      if (10 !== char) {
+        console.log(String.fromCharCode(char));
+      }
+    });
+  }
+});
