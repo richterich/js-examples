42affccfa07e57f616cc3d544f730da34155624e
Alexandr Rihtarich <rihtarich.ayu@gmail.com>
2020-05-19 20:55:09 +0300

    for iterator

--- example_6/for_iterator.js
+++ example_6/for_iterator.js
@@ -4,8 +4,8 @@
 process.stdin.on("readable", () => {
   let chunk;
   while (null !== (chunk = process.stdin.read())) {
-    for (char of chunk) {
-      if (10 !== char) {
+    for (let char of chunk) {
+      if (10 !== char && 13 !== char) {
         console.log(String.fromCharCode(char));
       }
     }
