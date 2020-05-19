efd4599b88033a0a93cf4035fcefec52e8a49e5a
Alexandr Rihtarich <rihtarich.ayu@gmail.com>
2020-05-19 21:03:53 +0300

    foreach method

--- example_6/foreach.js
+++ example_6/foreach.js
@@ -5,7 +5,7 @@ process.stdin.on("readable", () => {
   let chunk;
   while (null !== (chunk = process.stdin.read())) {
     chunk.forEach((char) => {
-      if (10 !== char) {
+      if (10 !== char && 13 !== char) {
         console.log(String.fromCharCode(char));
       }
     });
