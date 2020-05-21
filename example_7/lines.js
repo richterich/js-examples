7255d64b29fbfba4c66436c9f5979468fb832ce9
Alexandr Rihtarich <rihtarich.ayu@gmail.com>
2020-05-21 10:52:20 +0300

    one word per line

---
+++ example_7/lines.js
@@ -0,0 +1,20 @@
+/**
+ * prints one word per line
+ */
+process.stdin.on("readable", () => {
+  let chunk;
+  while (null !== (chunk = process.stdin.read())) {
+    let prev, line = '';
+    for (let char of chunk) {
+      if (32 === char || 9 === char) {
+        if (prev !== 32 && prev !== 9) {
+          line += '\n';
+        }
+      } else if (10 !== char && 13 !== char) {
+        line += String.fromCharCode(char);
+      }
+      prev = char;
+    }
+    console.log(line);
+  }
+});
