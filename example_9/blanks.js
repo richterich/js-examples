56e437a52dfcfd219c8f58f7b1eeb297b358e529
Rihtarich <rihtarich.ayu@gmail.com>
2020-05-23 22:48:41 +0300

    a single blank

---
+++ example_9/blanks.js
@@ -0,0 +1,18 @@
+/**
+  * replacing one or more blanks by a single blank.
+  */
+process.stdin.on("readable", () => {
+  let chunk;
+  while (null !== (chunk = process.stdin.read())) {
+    let last, line = '';
+    for (let char of chunk) {
+      if (10 !== char && 13 !== char) {
+        if (32 !== last || 32 !== char) {
+          line += String.fromCharCode(char);
+          last = char;
+        }
+      }
+    }
+    console.log(line);
+  }
+});
