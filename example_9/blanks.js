08f94c392273e23a8001d886c0f3d75d1994cf97
Alexandr Rihtarich <rihtarich.ayu@gmail.com>
2020-05-25 22:44:49 +0300

    a single blank

--- example_9/blanks.js
+++ example_9/blanks.js
@@ -1,10 +1,10 @@
 /**
-  * replacing one or more blanks by a single blank.
-  */
+ * replacing one or more blanks by a single blank.
+ */
 process.stdin.on("readable", () => {
   let chunk;
   while (null !== (chunk = process.stdin.read())) {
-    let last, line = '';
+    let last, line = "";
     for (let char of chunk) {
       if (10 !== char && 13 !== char) {
         if (32 !== last || 32 !== char) {
