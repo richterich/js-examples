bada12d61babfcb206dbebe72576741f5f24d0d4
Rihtarich <rihtarich.ayu@gmail.com>
2020-06-08 19:52:06 +0300

    for iterator (format code)

--- example_6/for_iterator.js
+++ example_6/for_iterator.js
@@ -1,13 +1,13 @@
 /**
-  * prints one letter per line
-  */
-process.stdin.on("readable", () => {
-  let chunk;
-  while (null !== (chunk = process.stdin.read())) {
-    for (let char of chunk) {
-      if (10 !== char && 13 !== char) {
-        console.log(String.fromCharCode(char));
-      }
+ * prints one letter per line
+ */
+process.stdin.on('readable', () => {
+    let chunk;
+    while (null !== (chunk = process.stdin.read())) {
+        for (let char of chunk) {
+            if (10 !== char && 13 !== char) {
+                console.log(String.fromCharCode(char));
+            }
+        }
     }
-  }
 });
