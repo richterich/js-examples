40ea104261044f4584635cc944b56f677321a6c8
Rihtarich <rihtarich.ayu@gmail.com>
2020-06-10 23:05:01 +0300

    foreach method (format code)

--- example_6/foreach.js
+++ example_6/foreach.js
@@ -1,13 +1,13 @@
 /**
-  * prints one letter per line
-  */
-process.stdin.on("readable", () => {
-  let chunk;
-  while (null !== (chunk = process.stdin.read())) {
-    chunk.forEach((char) => {
-      if (10 !== char && 13 !== char) {
-        console.log(String.fromCharCode(char));
-      }
-    });
-  }
+ * prints one letter per line
+ */
+process.stdin.on('readable', () => {
+    let chunk;
+    while (null !== (chunk = process.stdin.read())) {
+        chunk.forEach((char) => {
+            if (10 !== char && 13 !== char) {
+                console.log(String.fromCharCode(char));
+            }
+        });
+    }
 });
