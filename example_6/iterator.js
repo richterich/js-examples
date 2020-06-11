324248f755f22e7b40609ad746e8af09577509e3
Rihtarich <rihtarich.ayu@gmail.com>
2020-06-11 21:18:21 +0300

    iterator (format code)

--- example_6/iterator.js
+++ example_6/iterator.js
@@ -1,14 +1,14 @@
 /**
-  * prints one letter per line
-  */
-process.stdin.on("readable", () => {
-  let chunk;
-  while (null !== (chunk = process.stdin.read())) {
-    let iterator = chunk[Symbol.iterator]();
-    let symbol = iterator.next();
-    while (!symbol.done && 10 !== symbol.value && 13 !== symbol.value) {
-      console.log(String.fromCharCode(symbol.value));
-      symbol = iterator.next();
+ * prints one letter per line
+ */
+process.stdin.on('readable', () => {
+    let chunk;
+    while (null !== (chunk = process.stdin.read())) {
+        let iterator = chunk[Symbol.iterator]();
+        let symbol = iterator.next();
+        while (!symbol.done && 10 !== symbol.value && 13 !== symbol.value) {
+            console.log(String.fromCharCode(symbol.value));
+            symbol = iterator.next();
+        }
     }
-  }
 });
