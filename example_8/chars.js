8974ab207a2e4539716c0335483ba76909bd79a2
Rihtarich <rihtarich.ayu@gmail.com>
2020-06-13 01:08:24 +0300

    different characters in input (format code)

--- example_8/chars.js
+++ example_8/chars.js
@@ -1,21 +1,21 @@
 /**
-  * prints amount of different characters in input
-  */
-process.stdin.on("readable", () => {
-  let chunk;
-  while (null !== (chunk = process.stdin.read())) {
-    let stats = { tabs: 0, spaces: 0, chars: 0, lines: 0 };
-    for (let char of chunk) {
-      if (32 === char) {
-        ++stats.spaces;
-      } else if (10 === char || 13 === char) {
-        ++stats.lines;
-      } else if (9 === char) {
-        ++stats.tabs;
-      } else {
-        ++stats.chars;
-      }
+ * prints amount of different characters in input
+ */
+process.stdin.on('readable', () => {
+    let chunk;
+    while (null !== (chunk = process.stdin.read())) {
+        let stats = { tabs: 0, spaces: 0, chars: 0, lines: 0 };
+        for (let char of chunk) {
+            if (32 === char) {
+                ++stats.spaces;
+            } else if (10 === char || 13 === char) {
+                ++stats.lines;
+            } else if (9 === char) {
+                ++stats.tabs;
+            } else {
+                ++stats.chars;
+            }
+        }
+        console.table(stats);
     }
-    console.table(stats);
-  }
 });
